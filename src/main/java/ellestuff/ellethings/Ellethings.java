dba4976e97984a55902cbc3505aa8554dc6443c9
ellestuff <ellef1566@gmail.com>
2023-11-22 00:23:30 +0000

    idk

---
+++ src/main/java/ellestuff/ellethings/Ellethings.java
@@ -0,0 +1,13 @@
+package ellestuff.ellethings;
+
+import net.fabricmc.api.ModInitializer;
+
+public class Ellethings implements ModInitializer {
+    /**
+     * Runs the mod initializer.
+     */
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/ellestuff/ellethings/client/EllethingsClient.java
@@ -0,0 +1,13 @@
+package ellestuff.ellethings.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class EllethingsClient implements ClientModInitializer {
+    /**
+     * Runs the mod initializer on the client environment.
+     */
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

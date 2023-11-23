6acd42343a2fae635fd45917c1d75f9a5332214c
ellestuff <ellef1566@gmail.com>
2023-11-23 14:34:33 +0000

    i dunno

--- src/main/java/ellestuff/ellethings/ElleThings.java
+++ src/main/java/ellestuff/ellethings/ElleThings.java
@@ -1,8 +1,12 @@
 package ellestuff.ellethings;
 
+import ellestuff.ellethings.items.ThrowingGloveItem;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.item.Items;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
@@ -10,14 +14,21 @@ import net.minecraft.util.Identifier;
 public class ElleThings implements ModInitializer {
     public static final String MODID = "ellethings";
 
-    public static final Item SLIME_THROWER = new Item(new FabricItemSettings());
-    public static final Item MAGMA_THROWER = new Item(new FabricItemSettings());
+    public static final Item THROWING_GLOVE = new ThrowingGloveItem(new FabricItemSettings());
+    public static final Item NETHERITE_THROWING_GLOVE = new Item(new FabricItemSettings());
 
     @Override
     public void onInitialize() {
         { // Item Registering
-            Registry.register(Registries.ITEM, new Identifier(MODID, "slime_thrower"), SLIME_THROWER);
-            Registry.register(Registries.ITEM, new Identifier(MODID, "magma_thrower"), MAGMA_THROWER);
+            Registry.register(Registries.ITEM, new Identifier(MODID, "throwing_glove"), THROWING_GLOVE);
+            Registry.register(Registries.ITEM, new Identifier(MODID, "netherite_throwing_glove"), NETHERITE_THROWING_GLOVE);
         } // Item Registering
+
+        { // Creative menu listings
+            ItemGroupEvents.modifyEntriesEvent(ItemGroups.COMBAT).register(content -> {
+                content.addAfter(Items.BOW, THROWING_GLOVE);
+                content.addAfter(THROWING_GLOVE, NETHERITE_THROWING_GLOVE);
+            });
+        }
     }
 }
---
+++ src/main/java/ellestuff/ellethings/items/ThrowingGloveItem.java
@@ -0,0 +1,124 @@
+package ellestuff.ellethings.items;
+
+import java.util.function.Predicate;
+import net.minecraft.enchantment.EnchantmentHelper;
+import net.minecraft.enchantment.Enchantments;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity;
+import net.minecraft.entity.projectile.PersistentProjectileEntity.PickupPermission;
+import net.minecraft.item.*;
+import net.minecraft.registry.tag.ItemTags;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
+import net.minecraft.stat.Stats;
+import net.minecraft.util.Hand;
+import net.minecraft.util.TypedActionResult;
+import net.minecraft.util.UseAction;
+import net.minecraft.world.World;
+
+public class ThrowingGloveItem extends RangedWeaponItem implements Vanishable {
+
+    public static final Predicate<ItemStack> THROWER_PROJECTILES = (stack) -> {
+        return stack.isOf(Items.SLIME_BALL);
+    };
+    public static final int TICKS_PER_SECOND = 20;
+    public static final int RANGE = 15;
+
+    public ThrowingGloveItem(Item.Settings settings) {
+        super(settings);
+    }
+
+    public void onStoppedUsing(ItemStack stack, World world, LivingEntity user, int remainingUseTicks) {
+        if (user instanceof PlayerEntity playerEntity) {
+            boolean bl = playerEntity.getAbilities().creativeMode || EnchantmentHelper.getLevel(Enchantments.INFINITY, stack) > 0;
+            ItemStack itemStack = playerEntity.getProjectileType(stack);
+            if (!itemStack.isEmpty() || bl) {
+                if (itemStack.isEmpty()) {
+                    itemStack = new ItemStack(Items.SLIME_BALL);
+                }
+
+                int i = this.getMaxUseTime(stack) - remainingUseTicks;
+                float f = getPullProgress(i);
+                if (!((double)f < 0.1)) {
+                    boolean bl2 = bl && itemStack.isOf(Items.SLIME_BALL);
+                    if (!world.isClient) {
+                        ArrowItem thrownItem = (ArrowItem)(itemStack.getItem() instanceof ArrowItem ? itemStack.getItem() : Items.SLIME_BALL);
+                        PersistentProjectileEntity persistentProjectileEntity = thrownItem.createArrow(world, itemStack, playerEntity);
+                        persistentProjectileEntity.setVelocity(playerEntity, playerEntity.getPitch(), playerEntity.getYaw(), 0.0F, f * 3.0F, 1.0F);
+                        if (f == 1.0F) {
+                            persistentProjectileEntity.setCritical(true);
+                        }
+
+                        int j = EnchantmentHelper.getLevel(Enchantments.POWER, stack);
+                        if (j > 0) {
+                            persistentProjectileEntity.setDamage(persistentProjectileEntity.getDamage() + (double)j * 0.5 + 0.5);
+                        }
+
+                        int k = EnchantmentHelper.getLevel(Enchantments.PUNCH, stack);
+                        if (k > 0) {
+                            persistentProjectileEntity.setPunch(k);
+                        }
+
+                        if (EnchantmentHelper.getLevel(Enchantments.FLAME, stack) > 0) {
+                            persistentProjectileEntity.setOnFireFor(100);
+                        }
+
+                        stack.damage(1, playerEntity, (p) -> {
+                            p.sendToolBreakStatus(playerEntity.getActiveHand());
+                        });
+
+                        world.spawnEntity(persistentProjectileEntity);
+                    }
+
+                    world.playSound((PlayerEntity)null, playerEntity.getX(), playerEntity.getY(), playerEntity.getZ(), SoundEvents.ENTITY_SNOWBALL_THROW, SoundCategory.PLAYERS, 1.0F, 1.0F / (world.getRandom().nextFloat() * 0.4F + 1.2F) + f * 0.5F);
+                    if (!bl2 && !playerEntity.getAbilities().creativeMode) {
+                        itemStack.decrement(1);
+                        if (itemStack.isEmpty()) {
+                            playerEntity.getInventory().removeOne(itemStack);
+                        }
+                    }
+
+                    playerEntity.incrementStat(Stats.USED.getOrCreateStat(this));
+                }
+            }
+        }
+    }
+
+    public static float getPullProgress(int useTicks) {
+        float f = (float)useTicks / 20.0F;
+        f = (f * f + f * 2.0F) / 3.0F;
+        if (f > 1.0F) {
+            f = 1.0F;
+        }
+
+        return f;
+    }
+
+    public int getMaxUseTime(ItemStack stack) {
+        return 72000;
+    }
+
+    public UseAction getUseAction(ItemStack stack) {
+        return UseAction.BOW;
+    }
+
+    public TypedActionResult<ItemStack> use(World world, PlayerEntity user, Hand hand) {
+        ItemStack itemStack = user.getStackInHand(hand);
+        boolean bl = !user.getProjectileType(itemStack).isEmpty();
+        if (!user.getAbilities().creativeMode && !bl) {
+            return TypedActionResult.fail(itemStack);
+        } else {
+            user.setCurrentHand(hand);
+            return TypedActionResult.consume(itemStack);
+        }
+    }
+
+    public Predicate<ItemStack> getProjectiles() {
+        return THROWER_PROJECTILES;
+    }
+
+    public int getRange() {
+        return 15;
+    }
+}

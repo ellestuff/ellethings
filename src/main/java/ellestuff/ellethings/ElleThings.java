4b82df21529cda56e34a39da5ca5da1c09604edd
ellestuff <ellef1566@gmail.com>
2023-11-23 00:59:50 +0000

    adding things

--- src/main/java/ellestuff/ellethings/ElleThings.java
+++ src/main/java/ellestuff/ellethings/ElleThings.java
@@ -1,13 +1,24 @@
 package ellestuff.ellethings;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.fabricmc.loader.api.ModContainer;
+import net.minecraft.item.Item;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
 
 public class ElleThings implements ModInitializer {
-    /**
-     * Runs the mod initializer.
-     */
+    public static final String MODID = "ellethings";
+
+    public static final Item SLIME_THROWER = new Item(new FabricItemSettings());
+    public static final Item MAGMA_THROWER = new Item(new FabricItemSettings());
+
     @Override
     public void onInitialize() {
-    
+        { // Item Registering
+            Registry.register(Registries.ITEM, new Identifier(MODID, "slime_thrower"), SLIME_THROWER);
+            Registry.register(Registries.ITEM, new Identifier(MODID, "magma_thrower"), MAGMA_THROWER);
+        } // Item Registering
     }
 }
--- src/main/java/ellestuff/ellethings/client/ElleClient.java
+++ src/main/java/ellestuff/ellethings/client/ElleClient.java
@@ -2,7 +2,7 @@ package ellestuff.ellethings.client;
 
 import net.fabricmc.api.ClientModInitializer;
 
-public class ElleThingsClient implements ClientModInitializer {
+public class ElleClient implements ClientModInitializer {
     /**
      * Runs the mod initializer on the client environment.
      */

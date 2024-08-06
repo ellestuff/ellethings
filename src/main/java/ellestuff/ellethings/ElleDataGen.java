c498d447c96daa5a9a95a9c214d85c1364c7baf4
ellestuff. <43822029+ellestuff@users.noreply.github.com>
2024-08-06 15:09:19 +0100

    how do i use ElleTrimMaterials

--- src/main/java/ellestuff/ellethings/ElleDataGen.java
+++ src/main/java/ellestuff/ellethings/ElleDataGen.java
@@ -1,15 +1,12 @@
 package ellestuff.ellethings;
 
+import ellestuff.ellethings.datagen.ElleTrimMaterials;
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
 
 public class ElleDataGen implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
-
-        // Adding a provider example:
-        //
-        // pack.addProvider(AdvancementsProvider::new);
-
     }
 }

280876513b3aa56071b79fc3c4f3851c9207e4f9
ellestuff. <43822029+ellestuff@users.noreply.github.com>
2024-08-06 17:08:03 +0100

    aaaaaa

--- src/main/java/ellestuff/ellethings/ElleDataGen.java
+++ src/main/java/ellestuff/ellethings/ElleDataGen.java
@@ -3,10 +3,17 @@ package ellestuff.ellethings;
 import ellestuff.ellethings.datagen.ElleTrimMaterials;
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+import net.minecraft.registry.RegistryBuilder;
+import net.minecraft.registry.RegistryKeys;
 
 public class ElleDataGen implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator generator) {
         FabricDataGenerator.Pack pack = generator.createPack();
+    }
+
+    @Override
+    public void buildRegistry(RegistryBuilder registryBuilder) {
+        registryBuilder.addRegistry(RegistryKeys.TRIM_MATERIAL, ElleTrimMaterials::bootstrapType);
     }
 }
